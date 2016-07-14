96b7a6da71e67dd6f401ba24d0cbde3cc302bda0
patriciarealini <hello@patriciarealini.com>
2016-07-14 14:08:57 -0700

    excluir vírgulas

--- client/index.jsx
+++ client/index.jsx
@@ -1,12 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
 
-import {Rolodex} from "./components/index.jsx";
+import {Rolodex} from "./components/index.jsx"
 
 ReactDOM.render(
   <div>
-    <h1>Rolodex</h1>
     <Rolodex />
   </div>,
   document.getElementById('rolodex')
-);
+)
